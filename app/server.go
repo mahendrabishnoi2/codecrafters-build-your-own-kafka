7ed19a82df1437578eb4998953c546259951e565
Mahendra Bishnoi <mahendrabishnoi2@gmail.com>
2024-12-05 21:34:52 +0530

    sending correlation id

--- app/server.go
+++ app/server.go
@@ -7,10 +7,6 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
